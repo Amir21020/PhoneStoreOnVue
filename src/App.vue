d11a15768bb3610fc98722aaeb4c59ea46827eca
Amir Amirov <mergalievradmir484@gmail.com>
2025-01-25 13:47:06 +0300

    Add CardComponent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
 import Header from './components/Header.vue';
-
+import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+  <div class="w-4/5 m-auto bg-white  rounded-xl shadow-xl mt-14">
     <Header />
 
-    <div>
-      <div>
-        <img src="/like-1.svg" alt="Like 1">
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker">
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+      <div class="grid grid-cols-4 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
+
   </div>
 </template>
 
